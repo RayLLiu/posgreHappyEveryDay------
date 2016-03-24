35b5be866c6d3aa8f2c6c487208125459c8bd340
KingOfPoutine <rogerliuray@gmail.com>
2016-03-24 18:09:17 -0400

    Email hot on fire check
    I still haven’t figured out why npm crashes when running in non-test
    mode

--- Test/routes/index.js
+++ Test/routes/index.js
@@ -1,26 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var session = require('client-sessions');
+var pg = require('pg');
+var deepEqual = require('deep-equal');
+var connectionString = 'postgres://rliu040:8439L177Lr@@web0.site.uottawa.ca:15432/rliu040';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('login',{nullinput:''});
+  res.render('login', {
+    nullinput: ''
+  });
 });
 
 /*Get sign up page*/
 router.get('/signup', function(req, res, next) {
   res.render('signup');
-  
+
 });
 
-/* Process login requests*/
-router.get('/login', function(req, res, next) {
-   var email = req.body.email;
-  var password = req.body.password;
-    console.log(email + "***************" + password);
-   
 
-   if (email != "" && password != "") {
+
+/* Process login requests*/
+router.post('/login', function(req, res, next) {
+  var email = req.body.email;
+  var password = req.body.password;
+  console.log(email + "***************" + password);
+
+
+  if (email != "" && password != "") {
     // Get a Postgres client from the connection pool
+    console.log("sajoisjdosidjfoajsdofjaosdifjoasidjoiasjdoisajdoooadsj");
+
     pg.connect(connectionString, function(err, client, done) {
       // Handle connection errors
 
@@ -47,11 +57,13 @@ router.get('/login', function(req, res, next) {
         /**
         if the password is correct, direct the user to user page:
         */
+        console.log(result.rows[0].password);
         var a = result.rows[0].password;
         var S = require('string');
         var b = S(a).strip(' ').s;
         if ([deepEqual(b, password)]) {
           console.log("*********Login successfully*****");
+          //req.session.user = user;
           // redirect the page to user's info page
           res.render('user');
           res.end("yes");
@@ -63,12 +75,73 @@ router.get('/login', function(req, res, next) {
         console.log("the password is" + result.rows[0].password);
       });
     });
-  }
-  else {
+  } else {
     //if no input for Username and pw, throw a new login page
-    res.render('login',{nullinput:'Please enter the email and password.'});
+    res.render('login', {
+      nullinput: 'Please enter the email and password.'
+    });
   }
 });
+
+
+
+/* Process Signup requests*/
+router.get('/signup_submit', function(req, res, next) {
+  var email = req.body.email;
+  var password = req.body.password;
+  var first_name = req.body.first_name;
+  var last_name = req.body.last_name;
+  console.log(email + "***************" + password);
+
+
+  if (email != "" && password != "") {
+    // Get a Postgres client from the connection pool
+    pg.connect(connectionString, function(err, client, done) {
+      // Handle connection errors
+
+      if (err) {
+        done();
+        console.log(err);
+        return res.status(500).json({
+          success: false,
+          data: err
+        });
+      }
+
+      // SQL Query >insert user into db
+      client.query("SET SEARCH_PATH='movedb';");
+      var q = "INSERT INTO USERS(password,last_name,first_name,email) VALUES('" + password + "','" + last_name + "','" + first_name + "','" + email + "')";
+
+
+
+      client.query(q, function(err, result) {
+        if (err) {
+          return console.error('error running query', err);
+        }
+
+          console.log("sign in successfully");
+        res.end("yes");
+
+
+
+      });
+    });
+  }
+
+});
+/*   Sign up helper function*/
+/*check if the email exists*/
+router.get('/check_emil_exists', function(req, res, next) {
+  res.render('login', {
+    nullinput: ''
+  });
+  console.log("hudihudhisudsud");
+  var email=req.data.email;
+
+
+
+});
+
 
 /*G*/
 
