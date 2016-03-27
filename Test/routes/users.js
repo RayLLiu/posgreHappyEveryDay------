1bef0c34fc029b0728fe437c33ddff332ac49d37
KingOfPoutine <rogerliuray@gmail.com>
2016-03-26 21:33:09 -0400

    back end frame done

--- Test/routes/users.js
+++ Test/routes/users.js
@@ -48,5 +48,116 @@ router.get('/get_movie_list', function(req, res, next) {
     res.send(result);
   });
 });
-
+//c
+router.get('/get_actors_from_movie', function(req, res, next) {
+  console.log("actor and role");
+  db.get_user_list(function(err, result) {
+    console.log(result);
+    res.send(result);
+  });
+});
+//d
+router.get('/actor_appear_most', function(req, res, next) {
+  console.log("movie");
+  db.get_user_list(function(err, result) {
+    console.log(result);
+    res.send(result);
+  });
+});
+//e
+router.get('/two_most_actors', function(req, res, next) {
+  console.log("movie");
+  db.get_user_list(function(err, result) {
+    console.log(result);
+    res.send(result);
+  });
+});
+//f
+router.get('/ten_highest_rating_movies', function(req, res, next) {
+  console.log("movie");
+  db.get_user_list(function(err, result) {
+    console.log(result);
+    res.send(result);
+  });
+});
+//g
+router.get('/hiest_rating_movie_and_topic', function(req, res, next) {
+  console.log("movie");
+  db.get_user_list(function(err, result) {
+    console.log(result);
+    res.send(result);
+  });
+});
+//h
+router.get('/user_rating_number', function(req, res, next) {
+  console.log("movie");
+  db.get_user_list(function(err, result) {
+    console.log(result);
+    res.send(result);
+  });
+});
+//i
+router.get('/movie_not_rated_2016', function(req, res, next) {
+  console.log("movie");
+  db.get_user_list(function(err, result) {
+    console.log(result);
+    res.send(result);
+  });
+});
+//j
+router.get('/director_movie_lower', function(req, res, next) {
+  console.log("movie");
+  db.get_user_list(function(err, result) {
+    console.log(result);
+    res.send(result);
+  });
+});
+//k
+router.get('/category_highest_movie', function(req, res, next) {
+  console.log("category name");
+  db.type_highest_movie([req.body.name],function(err, result) {
+    console.log(result);
+    res.send(result);
+  });
+});
+//l
+router.get('/category_most_popular', function(req, res, next) {
+  console.log("category name");
+  db.type_highest_movie([req.body.name],function(err, result) {
+    console.log(result);
+    res.send(result);
+  });
+});
+//m
+router.get('/user_overall_rating', function(req, res, next) {
+  console.log("category name");
+  db.type_highest_movie([req.body.name],function(err, result) {
+    console.log(result);
+    res.send(result);
+  });
+});
+//n
+router.get('/frequent_user', function(req, res, next) {
+  console.log("category name");
+  db.type_highest_movie([req.body.name],function(err, result) {
+    console.log(result);
+    res.send(result);
+  });
+});
+//o
+router.get('/John_Smith', function(req, res, next) {
+  console.log("category name");
+  db.type_highest_movie([req.body.username],function(err, result) {
+    console.log(result);
+    res.send(result);
+  });
+});
+//p
+router.get('/diverse_user', function(req, res, next) {
+  console.log("category name");
+  db.type_highest_movie([req.body.username],function(err, result) {
+    console.log(result);
+    res.send(result);
+  });
+});
 module.exports = router;
