8484278a468e3c44b608b7257c65bdb0f9e2650c
KingOfPoutine <rogerliuray@gmail.com>
2016-04-02 19:34:37 -0400

    save

--- JAVA/src/convert.java
+++ JAVA/src/convert.java
@@ -24,42 +24,73 @@ public class convert {
 		// 2nd, parses all rows from the CSV file into a 2-dimensional array
 		List<String[]> resolvedData = parser.parseAll(new FileReader("/Users/rayliu/Desktop/movie.csv"));
 
-		System.out.println(resolvedData.get(10)[3]);
+		
 		String eol = System.getProperty("line.separator");
-		String content = resolvedData.get(10)[3];
-		String query=actor(resolvedData);
+		
+		String query = general(resolvedData, "movie");
 		String path = "/Users/rayliu/Desktop/movie.sql";
-		try{
-		Files.write( Paths.get(path), query.getBytes(), StandardOpenOption.CREATE);}
-		catch (IOException e){
+		try {
+			Files.write(Paths.get(path), query.getBytes(), StandardOpenOption.CREATE);
+		} catch (IOException e) {
 			System.out.println("IO ERROR");
 		}
 	}
-	
-	//help method
-	public static String actor(List<String[]> a){
+
+	// help method
+	public static String actor(List<String[]> a) {
 		String eol = System.getProperty("line.separator");
-		String query="";
-		for(int i=1;i<a.size();i++){
-		 query=query+eol+"INSERT INTO movie(actor_id,last_name,first_name,date_of_birth) VALUES(\'"+a.get(i)[0]+"\',\'"+a.get(i)[1]+"\',\'"+a.get(i)[2]+"\',\'Convert(DateTime,\'"+a.get(i)[3]+"\',126)\');";
-			
+		String query = "";
+		for (int i = 1; i < a.size(); i++) {
+			query = query + eol + "INSERT INTO movie(actor_id,last_name,first_name,date_of_birth) VALUES(\'"
+					+ a.get(i)[0] + "\',\'" + a.get(i)[1] + "\',\'" + a.get(i)[2] + "\',\'Convert(DateTime,\'"
+					+ a.get(i)[3] + "\',126)\');";
+
 		}
 		return query;
 	}
-	
-	
-	
-	public static String movie(String[] a ,String trailer){
-		
-		/*
-		 * INSERT INTO Customer(CustId,Name,Address,Amount) 
-VALUES (1,'John','Ottawa',8.5),
-(2,'Amy','Orleans',9.0),
-(3,'Peter','Gatineau',6.3);
-		 */
-		String query="INSERT INTO movie(movie_id,name,date_released,language,subtitles,country,trailer) VALUES(\'"+a[0]+"\',\'"+a[1]+"\',\'"+a[2]+"\',\'"+a[3]+"\',\'"+a[4]+"\',\'"+a[5]+"\',\'"+a[6]+"\')";
-		
-		
+
+	// help method
+	public static String movie(List<String[]> a) {
+		String eol = System.getProperty("line.separator");
+		String query = "";
+		for (int i = 1; i < a.size(); i++) {
+
+			query = query + eol
+					+ "INSERT INTO movie(movie_id,name,date_released,language,subtitles,country,trailer,image) VALUES(\'"
+					+ a.get(i)[0] + "\',\'" + a.get(i)[1] + "\',\'" + a.get(i)[2] + "\',\'" + a.get(i)[3] + "\',\'"
+					+ a.get(i)[4] + "\',\'" + a.get(i)[5] + "\',\'" + a.get(i)[6] + "\',\'" + a.get(i)[7] + "\');";
+
+		}
+
 		return query;
 	}
+
+	// get user
+	public static String general(List<String[]> a, String table) {
+		String title = "INSERT INTO " + table + "(";
+		for (int k = 0; k < a.get(0).length; k++) {
+			if (k == a.get(0).length - 1) {
+				title = title + a.get(0)[k];
+			} else {
+				title = title + a.get(0)[k] + ",";
+			}
+		}
+		title = title + ") VALUES(\'";
+		String eol = System.getProperty("line.separator");
+		String query = "";
+		for (int i = 1; i < a.size(); i++) {
+			query = query + eol + title;
+			for (int g = 0; g < a.get(0).length; g++) {
+				if(g == a.get(0).length - 1) {
+				query = query+ a.get(i)[g] + "\');";}
+				else{
+					query = query+a.get(i)[g] + "\',\'";
+				}
+
+			}
+		}
+
+		return query;
+	}
+
 }
